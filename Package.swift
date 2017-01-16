177ba359f487f8f4831e911c24638e605f6d431d
Donald Pinckney <donald_pinckney@icloud.com>
2017-01-16 01:24:28 -0800

    Update SwiftNum

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftLearn",
     dependencies: [
-        .Package(url: "https://github.com/donald-pinckney/SwiftNum", Version(1, 9, 7))
+        .Package(url: "https://github.com/donald-pinckney/SwiftNum", Version(1, 9, 8))
     ]
 )
